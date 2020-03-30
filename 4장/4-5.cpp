c2e377e277573c9b5d69dd41d4c545cf9e7741b0
username <qkrdydals327@gmail.com>
2020-03-30 17:33:05 +0900

    Update 2020 03 30

---
+++ 4장/4-5.cpp
@@ -0,0 +1,89 @@
+#include<Windows.h>
+#include<tchar.h>
+#include<stdio.h>
+#include"resource.h"
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow) {
+	HWND hwnd;
+	MSG msg;
+	WNDCLASS WndClass;
+
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hInstance = hInstance;
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.lpszMenuName = MAKEINTRESOURCE(IDR_MENU4_2);
+	WndClass.lpszClassName = _T("Window Class Name");
+	RegisterClass(&WndClass);
+
+	hwnd = CreateWindow(_T("Window Class Name"), _T("Window Title Name"), WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, NULL, hInstance, NULL);
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
+void OutFromFile(TCHAR filename[], HWND hwnd) {
+	FILE* fPtr;
+	HDC hdc;
+	int line;
+	TCHAR buffer[500];
+	line = 0;
+	hdc = GetDC(hwnd);
+	
+#ifdef _UNICODE
+	_tfopen_s(&fPtr, filename, _T("r, css = UNICODE"));
+#else
+	_tfopen_s(&fPtr, filename, _T("r"));
+#endif
+	while (_fgetts(buffer, 100, fPtr) != NULL) {
+		if (buffer[_tcslen(buffer) - 1] == _T('\n'))
+			buffer[_tcslen(buffer) - 1] = NULL;
+		TextOut(hdc, 0, line * 20, buffer, _tcslen(buffer));
+		line++;
+	}
+	fclose(fPtr);
+	ReleaseDC(hwnd, hdc);
+}
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam) {
+
+	OPENFILENAME OFN;
+	TCHAR str[100], lpstrFile[100] = _T("");
+	TCHAR filter[] = _T("Every File(*.*) \0*.*\0Text File\0*.txt;*.doc\0");
+
+	switch (iMsg) {
+	case WM_CREATE:
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case ID_FILEOPEN:
+			memset(&OFN, 0, sizeof(OPENFILENAME));
+			OFN.lStructSize = sizeof(OPENFILENAME);
+			OFN.hwndOwner = hwnd;
+			OFN.lpstrFilter = filter;
+			OFN.lpstrFile = lpstrFile;
+			OFN.nMaxFile = 100;
+			if (GetOpenFileName(&OFN) != 0)
+				OutFromFile(OFN.lpstrFile, hwnd);
+			break;
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return DefWindowProc(hwnd, iMsg, wParam, lParam);
+
+}
